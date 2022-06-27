120693e44a82d5ad985f027b680e396ea98dbc00
quentin <quentinurschfeld@gmail.com>
2022-06-27 14:49:37 +0200

    change index

--- src/main/java/hitema/com/bionic/BionicApplication.java
+++ src/main/java/hitema/com/bionic/BionicApplication.java
@@ -1,11 +1,11 @@
 package hitema.com.bionic;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BionicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BionicApplication.class, args);
 	}
