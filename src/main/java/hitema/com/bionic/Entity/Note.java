3c847db3fe0bf83b45e6c9d63d4abbfe7e5382c0
TRektFR <78340alex@gmail.com>
2022-06-27 14:48:30 +0200

    add Club, Note entities and modify user to add association between entities

---
+++ src/main/java/hitema/com/bionic/Entity/Note.java
@@ -0,0 +1,60 @@
+package hitema.com.bionic.Entity;
+
+import javax.persistence.*;
+
+@Entity
+    @Table(name = "Note")
+public class Note {
+    @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+            @Column(name="ID")
+    private int id;
+    @Column(name="User_ID")
+    private int user_id;
+    @Column(name="Club_ID")
+    private int club_id;
+    @Column(name="Texte")
+    private String note;
+    @Column(name="Note_Name")
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getClub_id() {
+        return club_id;
+    }
+
+    public void setClub_id(int club_id) {
+        this.club_id = club_id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- src/main/java/hitema/com/bionic/Entity/User.java
+++ src/main/java/hitema/com/bionic/Entity/User.java
@@ -2,6 +2,8 @@ package hitema.com.bionic.Entity;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
     @Table(name = "User")
@@ -11,12 +13,22 @@ public class User {
         @GeneratedValue(strategy = GenerationType.IDENTITY)
             @Column(name="ID")
     private int id;
+
+    @ManyToMany
+    @JoinTable( name = "User_Club",
+            joinColumns = @JoinColumn( name = "User_ID" ),
+            inverseJoinColumns = @JoinColumn( name = "Club_ID" ) )
+    private List<Club> clubs = new ArrayList<>();
+
     @Column(name="UserName")
     private String name;
     @Column(name="Password")
     private String password;
     @Column(name="Date_Create")
     private LocalDateTime creationDate;
+
+    @OneToMany( targetEntity=Note.class, mappedBy="Notes" )
+    private List<Note> notes = new ArrayList<>();
 
     public int getId() {
         return id;
@@ -32,6 +44,14 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Club> getClubs() {
+        return clubs;
+    }
+
+    public List<Note> getNotes(){
+        return notes;
     }
 
     public String getPassword() {
