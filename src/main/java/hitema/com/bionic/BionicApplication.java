7a024b8733267e170ab8de44db9dc268f7163c37
quentin <quentinurschfeld@gmail.com>
2022-06-27 10:54:41 +0200

    Initial commit

---
+++ src/main/java/hitema/com/bionic/BionicApplication.java
@@ -0,0 +1,13 @@
+package hitema.com.bionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BionicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hitema/com/bionic/BionicApplicationTests.java
@@ -0,0 +1,13 @@
+package hitema.com.bionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BionicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
