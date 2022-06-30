d51abfbe3d125182c105474974a4e645a616f3e7
Melan <melanie-.-bonnet@hotmail.fr>
2022-06-30 21:44:48 +0200

    problème mdp a vide passe problème regler plus commentaire

--- client/js/inscription.js
+++ client/js/inscription.js
@@ -8,11 +8,12 @@ subscribeButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = subscribeForm.username.value;
     const password = subscribeForm.password.value;
+    //le mots de passe ne doit pas être vide
     if (password != "") {
         console.log(username + " " + password)
         try {
-            //callwebservice et vas voir si une paire username/password existe
-            //si il en existe une ça se connecte sinon message d'erreur
+            //callwebservice et vas voir si le username existe deja
+            //renvoie false si deja dans la base sinon true et crée le user
             callWebService("/users/user/subscribe/username=" + username.toString() + "&password=" + password.toString(), inscription, 'GET', 1, insciptionFailed)
         } catch (e) {
             console.error(e);
@@ -20,23 +21,26 @@ subscribeButton.addEventListener("click", (e) => {
         }
     }
     else {
+        //mots de passe vide
         console.log("mots de passe incorrecte")
         subscribeErrorMsgPassword.style.opacity = 1;
     }
-
+    //inscrit le user
     function inscription(data){
+        //si le username n'existe pas le user est crée
         if (data){
 
             console.log("inscirption réussite")
             alert("You have successfully subscribed.");
             document.location.href="./login.html";
         }
+        //si le username existe message d'erreur "username not available
         else {
             console.log("existe deja")
             subscribeErrorMsgUsername.style.opacity = 1;
         }
     }
-
+    //fonction callwebservice fail
     function insciptionFailed(){
         console.log("inscription failed")
     }
