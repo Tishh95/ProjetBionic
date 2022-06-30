a00094aec65b2302ab8c1b17018464959bd4fb82
quentin <quentinurschfeld@gmail.com>
2022-06-30 17:19:36 +0200

    add display to club note

--- client/js/club.js
+++ client/js/club.js
@@ -1,6 +1,8 @@
 let club
 let addPerson = ' '
 let addNote = ' '
+let person = []
+let list
 function fill() {
     console.log('appel à la fonction fill()...')
     club = getProp('club',true)
@@ -8,35 +10,37 @@ function fill() {
     let userid = getProp('userid',false)
     console.log(userid)
     console.log(club)
-    callWebService("/clubs/userclub/",clubOk, 'GET',1)
+    callWebService("/clubs/userclub/",personOK, 'GET',1)
     callWebService("/notes/list", noteOk, 'GET',1)
+
 }
 
-function clubOk(data) {
-    let person = []
+function personOK(data) {
+    gperon = data
     for( k in data) {
         if(data[k].clubs.length > 0){
             for(j in data[k].clubs){
 
                 if(data[k].clubs[j].id == club.id) {
                     person.push(data[k])
+
                 }
             }
         } else {
         }
-
     }
     for(k in person){
         addPerson += '<div class="divNotes"> ' +
             '</br>' +
             '<td>'+person[k].name+'</td>' +
+        '<td>'+person[k].id+'</td>' +
             '</div></a>'
     }
     document.getElementById('userList').innerHTML += addPerson;
-
 }
 
 function noteOk(data) {
+    gnotes =data
     let notes = []
     for(i in data) {
         if(data[i].club_id == club.id) {
@@ -44,10 +48,19 @@ function noteOk(data) {
         }
     }
     for(k in notes){
-        addPerson += '<div class="divNotes"> ' +
+        addNote += '<div class="divNotes"> ' +
             '</br>' +
-            '<td>'+notes[k].title+'</td>' +
+            '<td><a  onclick="displayOneNote('+k+')">'+notes[k].title+'</td>' + '</br>' +
+            '<td>'+notes[k].note+'</td>' +
             '</div></a>'
     }
     document.getElementById('notesList').innerHTML += addNote;
+}
+
+
+function displayOneNote(id) {
+
+    let note = gnotes[id];
+    putProp('note',note,true)
+    post('watchNote.html')
 }
